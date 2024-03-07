c589beb37fc30ee56254b0e82c122ff8cfd47473
Alheta Wahyu M <alheta.matalarens@gmail.com>
2024-03-07 13:23:39 +0700

    done latihan menangkap gambar dengan camera

---
+++ Media/02-01-starter/lib/screen/camera_screen.dart
@@ -0,0 +1,103 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatefulWidget {
+  final List<CameraDescription> cameras;
+
+  const CameraScreen({super.key, required this.cameras});
+
+  @override
+  State<CameraScreen> createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  bool _isCameraInitialized = false;
+
+  CameraController? controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    onNewCameraSelected(widget.cameras.first);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData.dark(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Ambil Gambar"),
+          actions: [
+            IconButton(
+              onPressed: () => _onCameraSwitch(),
+              icon: const Icon(Icons.cameraswitch),
+            ),
+          ],
+        ),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              _isCameraInitialized
+                  ? CameraPreview(controller!)
+                  : const Center(child: CircularProgressIndicator()),
+              Align(
+                alignment: const Alignment(0, 0.95),
+                child: _actionWidget(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void onNewCameraSelected(CameraDescription cameraDescription) async {
+    final previousCameraController = controller;
+    final cameraController =
+        CameraController(cameraDescription, ResolutionPreset.medium);
+
+    await previousCameraController?.dispose();
+
+    try {
+      await cameraController.initialize();
+    } on CameraException catch (e) {
+      print('Error Initializing camera: $e');
+    }
+
+    if (mounted) {
+      setState(() {
+        controller = cameraController;
+        _isCameraInitialized = controller!.value.isInitialized;
+      });
+    }
+  }
+
+  Widget _actionWidget() {
+    return FloatingActionButton(
+      heroTag: "take-picture",
+      tooltip: "Ambil Gambar",
+      onPressed: () => _onCameraButtonClick(),
+      child: const Icon(Icons.camera_alt),
+    );
+  }
+
+  Future<void> _onCameraButtonClick() async {
+    final navigator = Navigator.of(context);
+    final image = await controller?.takePicture();
+
+    navigator.pop(image);
+  }
+
+  void _onCameraSwitch() {}
+}
