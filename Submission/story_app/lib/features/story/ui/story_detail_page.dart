0da3c2f37b7ac23386c1fe95e6db7786a9befb24
Alheta Wahyu M <alheta.matalarens@gmail.com>
2024-03-18 00:05:22 +0700

    fix: detail Story UI

--- Submission/story_app/lib/features/story/ui/story_detail_page.dart
+++ Submission/story_app/lib/features/story/ui/story_detail_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
 import 'package:story_app/features/story/provider/api_provider.dart';
-import 'package:story_app/features/story/widgets/card_story.dart';
+import 'package:story_app/utils/date_time_extension.dart';
 import 'package:story_app/utils/result_state.dart';
 
 class StoryDetailPage extends StatefulWidget {
@@ -23,11 +24,52 @@ class _StoryDetailPageState extends State<StoryDetailPage> {
             child: CircularProgressIndicator(),
           );
         } else if (provider.state == ResultState.hasData) {
-          return CardStory(
-              onTap: () {},
-              name: provider.storyDetailResult.story.name,
-              description: provider.storyDetailResult.story.description,
-              imgUrl: provider.storyDetailResult.story.photoUrl);
+          final photoUrl = provider.storyDetailResult.story.photoUrl;
+          final name = provider.storyDetailResult.story.name;
+          final description = provider.storyDetailResult.story.description;
+          final createdAt =
+              provider.storyDetailResult.story.createdAt.toIndFormat();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Detail'),
+            ),
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.network(
+                  photoUrl,
+                  fit: BoxFit.fitWidth,
+                  width: 100.w,
+                  height: 480,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        name,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge
+                            ?.apply(fontWeightDelta: 2),
+                      ),
+                      Text(
+                        description,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      SizedBox(height: 8,),
+                      Text(createdAt, style: Theme.of(context).textTheme.bodySmall,),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
         } else if (provider.state == ResultState.noData) {
           return Center(
             child: Material(
@@ -46,25 +88,6 @@ class _StoryDetailPageState extends State<StoryDetailPage> {
           );
         }
       },
-    );
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Stories'),
-      ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Icon(Icons.account_circle_rounded),
-              SizedBox(
-                width: 8,
-              ),
-              Text("name"),
-            ],
-          ),
-        ],
-      ),
     );
   }
 }
