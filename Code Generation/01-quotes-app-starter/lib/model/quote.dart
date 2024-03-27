90bd3d2b0e9af3bd4c0c9a2b58830e1111e3e811
Alheta Wahyu M <alheta.matalarens@gmail.com>
2024-03-27 13:53:47 +0700

    done Latihan Json Serialization Quote List App

---
+++ Code Generation/01-quotes-app-starter/lib/model/quote.dart
@@ -0,0 +1,24 @@
+
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+part 'quote.g.dart';
+
+@JsonSerializable()
+class Quote {
+  final String id;
+  final String en;
+  final String author;
+  final double rating;
+
+  Quote({
+    required this.id,
+    required this.en,
+    required this.author,
+    required this.rating,
+  });
+
+  factory Quote.fromJson(Map<String, dynamic> map) => _$QuoteFromJson(map);
+
+  Map<String, dynamic> toJson() => _$QuoteToJson(this);
+}
